38c4a630906fde205ce39797c1690c55825a0563
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-02-19 18:25:42 +0000

    New run_speed_tests.sh an analyse.py outputs a json with results

--- analyse.py
+++ analyse.py
@@ -1,5 +1,9 @@
 import glob
 import statistics
+import json
+
+algorithms = {}
+
 
 def parse_file(filename: str):
     functions = {}
@@ -18,18 +22,26 @@ def parse_file(filename: str):
             continue
 
         functions[current_function].append(int(line))
-
     return functions
 
-def analyse(functions):
+
+def analyse(filename, functions):
     for function in functions:
         print(f"{function}:")
         print(f"Mean: {round(statistics.mean(functions[function]))}")
         print(f"Median: {round(statistics.median(functions[function]))}")
         print()
+        if filename.replace("./results_", "") not in algorithms:
+            algorithms[filename.replace("./results_", "")] = {}
+        algorithms[filename.replace("./results_", "")][
+            function.replace("xkem_", "").replace("gkem_", "")
+        ] = round(statistics.mean(functions[function]))
 
 
 for filename in glob.iglob("./results_*"):
     functions = parse_file(filename)
     print(filename)
-    analyse(functions)
+    analyse(filename, functions)
+
+with open("test_speed_results.json", "w") as write_file:
+    json.dump(algorithms, write_file, indent=2)
