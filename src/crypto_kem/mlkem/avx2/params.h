0053290d5a73e58114dcf2e0db4b5fbe8cdf2036
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-03-02 17:13:13 +0000

    simplified params in mlkem avx2

--- src/crypto_kem/mlkem/avx2/params.h
+++ src/crypto_kem/mlkem/avx2/params.h
@@ -1,68 +1,30 @@
 #ifndef PARAMS_HM
 #define PARAMS_HM
 
-#ifndef MLKEM_K
-#define MLKEM_K 3	/* Change this for different security strengths */
-#endif
+// #define MLKEM_90S	/* Uncomment this if you want the 90S variant */
 
-//#define MLKEM_90S	/* Uncomment this if you want the 90S variant */
-
-/* Don't change parameters below this line */
-#if   (MLKEM_K == 2)
-#ifdef MLKEM_90S
-#define MLKEM_NAMESPACE(s) pqcrystals_MLKEM512_90s_avx2_##s
-#else
-#define MLKEM_NAMESPACE(s) pqcrystals_MLKEM512_avx2_##s
-#endif
-#elif (MLKEM_K == 3)
-#ifdef MLKEM_90S
-#define MLKEM_NAMESPACE(s) pqcrystals_MLKEM768_90s_avx2_##s
-#else
 #define MLKEM_NAMESPACE(s) pqcrystals_MLKEM768_avx2_##s
-#endif
-#elif (MLKEM_K == 4)
-#ifdef MLKEM_90S
-#define MLKEM_NAMESPACE(s) pqcrystals_MLKEM1024_90s_avx2_##s
-#else
-#define MLKEM_NAMESPACE(s) pqcrystals_MLKEM1024_avx2_##s
-#endif
-#else
-#error "MLKEM_K must be in {2,3,4}"
-#endif
 
+#define MLKEM_K 3
 #define MLKEM_N 256
 #define MLKEM_Q 3329
 
-#define MLKEM_SYMBYTES 32   /* size in bytes of hashes, and seeds */
-#define MLKEM_SSBYTES  32   /* size in bytes of shared key */
+#define MLKEM_SYMBYTES 32 /* size in bytes of hashes, and seeds */
+#define MLKEM_SSBYTES 32  /* size in bytes of shared key */
 
-#define MLKEM_POLYBYTES		384
-#define MLKEM_POLYVECBYTES	(MLKEM_K * MLKEM_POLYBYTES)
-
-#if MLKEM_K == 2
-#define MLKEM_ETA1 3
-#define MLKEM_POLYCOMPRESSEDBYTES    128
-#define MLKEM_POLYVECCOMPRESSEDBYTES (MLKEM_K * 320)
-#elif MLKEM_K == 3
 #define MLKEM_ETA1 2
-#define MLKEM_POLYCOMPRESSEDBYTES    128
-#define MLKEM_POLYVECCOMPRESSEDBYTES (MLKEM_K * 320)
-#elif MLKEM_K == 4
-#define MLKEM_ETA1 2
-#define MLKEM_POLYCOMPRESSEDBYTES    160
-#define MLKEM_POLYVECCOMPRESSEDBYTES (MLKEM_K * 352)
-#endif
-
 #define MLKEM_ETA2 2
 
-#define MLKEM_INDCPA_MSGBYTES       (MLKEM_SYMBYTES)
-#define MLKEM_INDCPA_PUBLICKEYBYTES (MLKEM_POLYVECBYTES + MLKEM_SYMBYTES)
-#define MLKEM_INDCPA_SECRETKEYBYTES (MLKEM_POLYVECBYTES)
-#define MLKEM_INDCPA_BYTES          (MLKEM_POLYVECCOMPRESSEDBYTES + MLKEM_POLYCOMPRESSEDBYTES)
-
-#define MLKEM_PUBLICKEYBYTES  (MLKEM_INDCPA_PUBLICKEYBYTES)
-/* 32 bytes of additional space to save H(pk) */
-#define MLKEM_SECRETKEYBYTES  (MLKEM_INDCPA_SECRETKEYBYTES + MLKEM_INDCPA_PUBLICKEYBYTES + 2*MLKEM_SYMBYTES)
-#define MLKEM_CIPHERTEXTBYTES (MLKEM_INDCPA_BYTES)
-
+#define MLKEM_ETA 2
+#define MLKEM_POLYBYTES 384
+#define MLKEM_POLYVECBYTES 1152
+#define MLKEM_POLYCOMPRESSEDBYTES 128
+#define MLKEM_POLYVECCOMPRESSEDBYTES 960
+#define MLKEM_INDCPA_MSGBYTES 32
+#define MLKEM_INDCPA_PUBLICKEYBYTES 1184
+#define MLKEM_INDCPA_SECRETKEYBYTES 1152
+#define MLKEM_INDCPA_BYTES 1088
+#define MLKEM_PUBLICKEYBYTES 1184
+#define MLKEM_SECRETKEYBYTES 2400
+#define MLKEM_CIPHERTEXTBYTES 1088
 #endif
