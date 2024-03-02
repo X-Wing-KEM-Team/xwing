fd0892f34f714aa320402051f007d93e8055c136
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-03-02 17:23:24 +0000

    Removed travis and improved coverage

--- runlcov.sh
+++ runlcov.sh
@@ -4,11 +4,11 @@ cd src/crypto_kem/xwing/ref
 
 make -B test/test_xkem_functionality CFLAGS="-O0 -g --coverage -l25519"
 ./test/test_xkem_functionality
-lcov -c -d . -o xwing.lcov
+lcov -c -d . -o ../../../../xwing.lcov
 lcov -z -d .
 rm test/test_xkem_functionality
 
-lcov -r xwing.lcov -o xwing.lcov \
+lcov -r ../../../../xwing.lcov -o ../../../../xwing.lcov \
     '*/test/test_xkem_functionality.c'
 
 exit 0
