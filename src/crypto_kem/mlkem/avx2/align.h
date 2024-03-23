082b94932f789fbdf55703738874c15769c78a40
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-03-23 16:22:27 +0000

    kcp/optimized1600AVX2

--- src/crypto_kem/mlkem/avx2/align.h
+++ src/crypto_kem/mlkem/avx2/align.h
@@ -4,6 +4,21 @@
 #include <stdint.h>
 #include <immintrin.h>
 
+
+#ifdef ALIGN
+#undef ALIGN
+#endif
+
+#if defined(__GNUC__)
+#define ALIGN(x) __attribute__ ((aligned(x)))
+#elif defined(_MSC_VER)
+#define ALIGN(x) __declspec(align(x))
+#elif defined(__ARMCC_VERSION)
+#define ALIGN(x) __align(x)
+#else
+#define ALIGN(x)
+#endif
+
 #define ALIGNED_UINT8(N)        \
     union {                     \
         uint8_t coeffs[N];      \
